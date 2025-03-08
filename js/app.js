dafc3d8c2e34c18e46998b6b2575a57f3175390c
Ally Robinson <ally@purplish.blue>
2025-03-08 14:50:07 -0500

    more groudwork for terminal

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
 import { Terminal } from "@xterm/xterm/lib/xterm.js"
 import * as cli from "cli"
+
 var term = new Terminal();
 term.open(document.getElementById('terminal'));
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
 
 var myCli = cli.get_cli()
 
-term.onKey(function (evnt) {
-  var key = evnt.key;
-  if (key == '"\u007f') {
-    key = '\b'
-  }
-  console.log(key)
+term.onData(function (key) {
   cli.event(myCli, key, term.write, term)
 })
 
+  cli.event(myCli, "startup", term.write, term)
